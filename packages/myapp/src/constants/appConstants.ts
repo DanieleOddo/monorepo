ac9c091726c0b190c669799cedcc20d79882e3a2
DanieleOddo <daniele.oddo@outlook.com>
2022-12-29 08:37:07 +0100

    Update with Directory STD

---
+++ packages/myapp/src/constants/appConstants.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = 'SUper app'
+export const WIDGET_LABEL = 'Widget'
+
+export const apiStatus = {
+    IDLE: 'IDLE',
+    PENDING: 'PENDING',
+    SUCCESS: 'SUCCESS',
+    ERROR: 'ERROR'
+}
